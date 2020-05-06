1f0d9abd5abea860b1a98cf33afd3e34a39fe477
plf14 <59637504+plf14@users.noreply.github.com>
2020-05-05 22:37:49 -0400

    cleanup

--- web_app/routes/golf_routes.py
+++ web_app/routes/golf_routes.py
@@ -3,13 +3,6 @@ from flask import Blueprint, render_template, request, flash, redirect
 from app.handicap import scores, calc_handicap
 from app.post import post_score, calc_differential
 
-from dotenv import load_dotenv
-import os
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from heapq import nsmallest
-from heapq import nlargest
-
 golf_routes = Blueprint("golf_routes", __name__)
 
 @golf_routes.route("/handicap/form")
@@ -37,28 +30,13 @@ def handicap_result():
         return render_template("handicap_form.html")
 
 @golf_routes.route("/post")
-def new_user():
+def new_score():
     print("VISITED THE POST SCORE PAGE...")
     # return "Sign Up for our Product! (TODO)"
     return render_template("post.html")
 
-# @golf_routes.route("/post/done", methods=["POST"])
-# def create_user():
-#     print("ENTERING SCORE...")
-#     print("FORM DATA:", dict(request.form)) #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
-#     user = dict(request.form)
-#     differential = (eval(user["score"])-eval(user["rating"]))*(113/eval(user["slope"]))
-#     post_score(user["email_address"], user["first_name"], user["last_name"], user["date"], user["course"], user["score"], user["rating"], user["slope"], differential)
-#     email=user["email_address"].lower()
-#     results = scores(email)
-#     flash(f"Your score of {user['score']} at {user['course']} on {user['date']} was entered successfully!", "success") #success = green color alert
-#     return render_template("handicap_result.html", email=email, results=results)
-
-
-
-
 @golf_routes.route("/post/done", methods=["POST"])
-def create_user():
+def enter_score():
     print("ENTERING SCORE...")
     print("FORM DATA:", dict(request.form)) #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
     user = dict(request.form)
